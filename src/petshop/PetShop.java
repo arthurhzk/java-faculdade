7755ed0b2988c65f69cb4867cd60156618252213
Arthur Henrique Zarpelon Kunz <arthurhkunz@MacBook-Air-de-Arthur.local>
2024-06-19 18:17:13 -0300

    first commit

---
+++ src/petshop/PetShop.java
@@ -0,0 +1,14 @@
+
+package petshop;
+
+import tela.FrmUsarioLogin;
+
+public class PetShop {
+
+    public static void main(String[] args) {
+        //rodar tela usuario login 
+        new FrmUsarioLogin().setVisible(true);
+        
+        
+    }
+}
