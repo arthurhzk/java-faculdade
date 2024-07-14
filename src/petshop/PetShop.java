6c5210c1cb6f2d2c3b58d7c35a905a3fcc57c59c
Eduardo Moreira Viter <moreiraviter@outlook.com>
2024-07-14 03:30:09 -0300

    banco de dados

--- src/petshop/PetShop.java
+++ src/petshop/PetShop.java
@@ -1,13 +1,19 @@
 
 package petshop;
 
+import apoio.ConexaoBD;
+import javax.swing.JOptionPane;
 import tela.FrmUsarioLogin;
 
 public class PetShop {
 
     public static void main(String[] args) {
-        //rodar tela usuario login 
-        new FrmUsarioLogin().setVisible(true);
+        // if e else para carregar o banco de dados e botar  janela na tela 
+        if (ConexaoBD.getInstance().getConnection()  != null){
+            new FrmUsarioLogin().setVisible(true);
+        } else{
+           JOptionPane.showMessageDialog(null,"Erro ao conectar no Banco De Dados.");
+        }
         
         
     }
